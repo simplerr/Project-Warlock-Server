3751eb91913c130356198814fcaa0fe9184da1cc
Axel Blackert <axel.blackert@gmail.com>
2013-03-20 11:58:36 +0100

    Fixed bug where the main thread didn't cleanup properly.

--- Console.cpp
+++ Console.cpp
@@ -6,9 +6,24 @@
 #include <iostream>
 #include <fstream>
 #include <process.h>
+#include "Runnable.h"
 
-// maximum mumber of lines the output console should have
+// Maximum mumber of lines the output console should have.
 static const WORD MAX_CONSOLE_LINES = 500;
+
+BOOL WINAPI ConsoleHandlerRoutine(DWORD dwCtrlType)
+{
+	if(dwCtrlType == CTRL_CLOSE_EVENT)
+	{
+		// Send WM_DESTROY message.
+		SendMessage(GLib::GlobalApp->GetHwnd(), WM_DESTROY, 0, 0);
+
+		// Give the main thread some time to cleanup.
+		Sleep(3000);
+	}
+
+	return FALSE;
+}
 
 Console::Console()
 {
@@ -22,72 +37,47 @@ Console::~Console()
 
 void Console::Startup()
 {
-
 	int hConHandle;
-
 	long lStdHandle;
-
 	CONSOLE_SCREEN_BUFFER_INFO coninfo;
-
 	FILE *fp;
 
 	// allocate a console for this app
-
 	AllocConsole();
 
 	// set the screen buffer to be big enough to let us scroll text
-
-	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE),
-
-		&coninfo);
-
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &coninfo);
 	coninfo.dwSize.Y = MAX_CONSOLE_LINES;
 
-	SetConsoleScreenBufferSize(GetStdHandle(STD_OUTPUT_HANDLE),
-
-		coninfo.dwSize);
+	SetConsoleScreenBufferSize(GetStdHandle(STD_OUTPUT_HANDLE), coninfo.dwSize);
 
 	// redirect unbuffered STDOUT to the console
-
 	lStdHandle = (long)GetStdHandle(STD_OUTPUT_HANDLE);
-
 	hConHandle = _open_osfhandle(lStdHandle, _O_TEXT);
-
 	fp = _fdopen( hConHandle, "w" );
-
 	*stdout = *fp;
-
 	setvbuf( stdout, NULL, _IONBF, 0 );
 
 	// redirect unbuffered STDIN to the console
-
 	lStdHandle = (long)GetStdHandle(STD_INPUT_HANDLE);
-
 	hConHandle = _open_osfhandle(lStdHandle, _O_TEXT);
-
 	fp = _fdopen( hConHandle, "r" );
-
 	*stdin = *fp;
-
 	setvbuf( stdin, NULL, _IONBF, 0 );
 
 	// redirect unbuffered STDERR to the console
-
 	lStdHandle = (long)GetStdHandle(STD_ERROR_HANDLE);
-
 	hConHandle = _open_osfhandle(lStdHandle, _O_TEXT);
-
 	fp = _fdopen( hConHandle, "w" );
-
 	*stderr = *fp;
-
 	setvbuf( stderr, NULL, _IONBF, 0 );
 
 	// make cout, wcout, cin, wcin, wcerr, cerr, wclog and clog
-
 	// point to console as well
-
 	std::ios::sync_with_stdio();
+
+	// Set console handler, to catch the shutdown event.
+	SetConsoleCtrlHandler(ConsoleHandlerRoutine, TRUE);
 }
 
 void Console::GetInput()
